45c2ce1799b40acbe0119ab4564a0824425b98c3
53Hertzl <2606134257@qq.com>
2021-02-23 22:46:50 +0800

    添加文件

---
+++ Git01/src/main/java/HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * @program: HelloGit
+ * @description:
+ * @author: 53Hertz
+ * @create: 2021-02-23 22:30
+ **/
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("哈哈啊哈哈哈哈");
+    }
+}
